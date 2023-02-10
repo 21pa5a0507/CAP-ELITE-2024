d30d3debe375839c70f3b0a08a229a1730127fd8
21pa5a0507 <118523217+21pa5a0507@users.noreply.github.com>
2023-02-10 16:48:55 +0530

    Create sum vs xor.java

---
+++ 10-02-2023/sum vs xor.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'sumXor' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER n as parameter.
+     */
+
+    public static long sumXor(long n) {
+   String s="";
+   while(n>0){
+       s +=n%2;
+       n=n/2;
+   }
+   int count=0;
+   for(char i:s.toCharArray()){
+       if(i=='0'){
+           count++;
+       }
+   }
+   if(count==0){
+       return 1;
+   }
+   return (long)Math.pow(2,count);
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.sumXor(n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
