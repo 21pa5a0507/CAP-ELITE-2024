ddc57c2d9901b8269eac597a2c71452636084041
21pa5a0507 <118523217+21pa5a0507@users.noreply.github.com>
2023-02-10 16:47:12 +0530

    Create Recursive Digit Sum.java

---
+++ 10-02-2023/Recursive Digit Sum.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class Result {
+
+    /*
+     * Complete the 'superDigit' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING n
+     *  2. INTEGER k
+     */
+
+    public static int superDigit(String n, int k) {
+        long sum =0;
+        
+        for(char ch : n.toCharArray())
+        {
+            sum += (ch-'0');
+        }
+        
+        sum *= k;
+        
+        System.out.println(sum);
+        
+        while(sum>9)
+        {
+            int temp = 0;
+            
+            while(sum!=0)
+            {
+                temp += sum%10;
+                sum /= 10;
+            }
+            
+            sum = temp;
+        }
+        
+        return (int)sum;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] firstMultipleInput = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        String n = firstMultipleInput[0];
+
+        int k = Integer.parseInt(firstMultipleInput[1]);
+
+        int result = Result.superDigit(n, k);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
